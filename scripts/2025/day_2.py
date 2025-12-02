ea1845e4dd321c1e3f6cd810ff4f32057890eab8
Célien Donzé <celien.donze@hefr.ch>
2025-12-02 08:33:24 +0100

    clean project

--- scripts/2025/day_2.py
+++ scripts/2025/day_2.py
@@ -1,40 +1,32 @@
 from itertools import batched
 from pathlib import Path
 
+from tqdm import tqdm
+
 input_path = Path(__file__).parent.parent.parent / "data/2025/day_2.txt"
 
 with open(input_path) as file:
     line = file.read().strip()
 
 total = 0
-for s in line.split(","):
+for s in tqdm(line.split(",")):
     a, b = s.split("-")
-    a = int(a)
-    b = int(b)
-    for n in range(a, b + 1):
+    for n in range(int(a), int(b) + 1):
         m = str(n)
-        if (
-            m[: len(m) // 2] == m[len(m) // 2 :]
-        ):  # detect repeat of 2 times the same number
+        if m[: len(m) // 2] == m[len(m) // 2 :]:
             total += n
 
 print("Part 1:", total)
 
 total = 0
-for s in line.split(","):
+for s in tqdm(line.split(",")):
     a, b = s.split("-")
-    a = int(a)
-    b = int(b)
-    for n in range(a, b + 1):
+    for n in range(int(a), int(b) + 1):
         m = str(n)
-        # detect repeat of n times the same number
         for size in range(1, len(m) // 2 + 1):
-            try:
-                b = batched(m, size, strict=True)
-                if len(set(b)) == 1:
-                    total += n
-                    break
-            except ValueError:
-                continue
+            b = batched(m, size)
+            if len(set(b)) == 1:
+                total += n
+                break
 
 print("Part 2:", total)
